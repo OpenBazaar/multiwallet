be114be32b8d781c2dca887c2391ba887d0f3a6d
Mike Greenberg <mg@nobulb.com>
2019-01-03 14:34:47 -0500

    [#46] Add exponential backoff to rotationManager

--- client/rotation_manager.go
+++ client/rotation_manager.go
@@ -4,26 +4,61 @@ import (
 	"net/http"
 	"net/url"
 	"sync"
+	"time"
 
 	"github.com/OpenBazaar/multiwallet/client/blockbook"
 	"golang.org/x/net/proxy"
 )
 
+var maximumBackoff = 30 * time.Second
+
+type healthState struct {
+	lastFailedAt    time.Time
+	backoffDuration time.Duration
+}
+
+func (h *healthState) markUnhealthy() {
+	var now = time.Now()
+	if now.Before(h.nextAvilable()) {
+		// can't be unhealthy before it's available
+		return
+	}
+	if now.Before(h.lastFailedAt.Add(5 * time.Minute)) {
+		h.backoffDuration *= 2
+		if h.backoffDuration > maximumBackoff {
+			h.backoffDuration = maximumBackoff
+		}
+	} else {
+		h.backoffDuration = 2 * time.Second
+	}
+	h.lastFailedAt = now
+}
+
+func (h *healthState) isHealthy() bool {
+	return time.Now().After(h.nextAvailable())
+}
+
+func (h *healthState) nextAvailable() time.Time {
+	return h.lastFailedAt.Add(h.backoffDuration)
+}
+
 const nilTarget = RotationTarget("")
 
-type RotationTarget string
-type rotationManager struct {
-	currentTarget RotationTarget
-	targetHealth  map[RotationTarget]struct{}
-	clientCache   map[RotationTarget]*blockbook.BlockBookClient
-	rotateLock    sync.RWMutex
-	waiter        sync.WaitGroup
-}
-type reqFunc func(string, string, []byte, url.Values) (*http.Response, error)
+type (
+	RotationTarget  string
+	rotationManager struct {
+		currentTarget RotationTarget
+		targetHealth  map[RotationTarget]*healthState
+		clientCache   map[RotationTarget]*blockbook.BlockBookClient
+		rotateLock    sync.RWMutex
+		waiter        sync.WaitGroup
+	}
+	reqFunc func(string, string, []byte, url.Values) (*http.Response, error)
+)
 
 func newRotationManager(targets []string, proxyDialer proxy.Dialer, doReq reqFunc) (*rotationManager, error) {
 	var (
-		targetHealth = make(map[RotationTarget]struct{})
+		targetHealth = make(map[RotationTarget]*healthState)
 		clients      = make(map[RotationTarget]*blockbook.BlockBookClient)
 	)
 	for _, apiUrl := range targets {
@@ -33,7 +68,7 @@ func newRotationManager(targets []string, proxyDialer proxy.Dialer, doReq reqFun
 		}
 		c.RequestFunc = doReq
 		clients[RotationTarget(apiUrl)] = c
-		targetHealth[RotationTarget(apiUrl)] = struct{}{}
+		targetHealth[RotationTarget(apiUrl)] = &healthState{}
 	}
 	m := &rotationManager{
 		clientCache:   clients,
@@ -73,17 +108,27 @@ func (r *rotationManager) StartCurrent() error {
 func (r *rotationManager) WaitUntilClosed() { r.waiter.Wait() }
 
 func (r *rotationManager) FailCurrent() {
-	// TODO: Update health state
+	r.targetHealth[r.currentTarget].markUnhealthy()
 	r.currentTarget = nilTarget
 }
 
 func (r *rotationManager) SelectNext() {
-	// TODO: Health check before return available target
 	if r.currentTarget == nilTarget {
-		for target := range r.targetHealth {
-			r.currentTarget = target
-			r.waiter.Add(1)
-			break
+		var nextAvailableAt time.Time
+		for {
+			if time.Now().Before(nextAvailableAt) {
+				continue
+			}
+			for target, health := range r.targetHealth {
+				if health.isHealthy() {
+					r.currentTarget = target
+					r.waiter.Add(1)
+					return
+				}
+				if health.nextAvailable().After(nextAvailableAt) {
+					nextAvailableAt = health.nextAvailable()
+				}
+			}
 		}
 	}
 }
