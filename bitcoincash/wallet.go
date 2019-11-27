a26c9956d6c57bd35c837f7b8d0613c2e03f01ed
Chris <chris@ob1.io>
2019-11-27 13:38:54 -0500

    Update codebase with needed ethereum changes

--- bitcoincash/wallet.go
+++ bitcoincash/wallet.go
@@ -4,9 +4,10 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
-	"github.com/op/go-logging"
 	"io"
 	"log"
+	"math/big"
+	"strconv"
 	"time"
 
 	wi "github.com/OpenBazaar/wallet-interface"
@@ -41,8 +42,14 @@ type BitcoinCashWallet struct {
 	mPubKey  *hd.ExtendedKey
 
 	exchangeRates wi.ExchangeRates
-	log           *logging.Logger
 }
+
+var (
+	BitcoinCashCurrencyDefinition = wi.CurrencyDefinition{
+		Code:         "BCH",
+		Divisibility: 8,
+	}
+)
 
 func NewBitcoinCashWallet(cfg config.CoinConfig, mnemonic string, params *chaincfg.Params, proxy proxy.Dialer, cache cache.Cacher, disableExchangeRates bool) (*BitcoinCashWallet, error) {
 	seed := bip39.NewSeed(mnemonic, "")
@@ -76,18 +83,7 @@ func NewBitcoinCashWallet(cfg config.CoinConfig, mnemonic string, params *chainc
 
 	fp := util.NewFeeProvider(cfg.MaxFee, cfg.HighFee, cfg.MediumFee, cfg.LowFee, exchangeRates)
 
-	return &BitcoinCashWallet{
-		db:            cfg.DB,
-		km:            km,
-		params:        params,
-		client:        c,
-		ws:            wm,
-		fp:            fp,
-		mPrivKey:      mPrivKey,
-		mPubKey:       mPubKey,
-		exchangeRates: exchangeRates,
-		log:           logging.MustGetLogger("bitcoin-cash-wallet"),
-	}, nil
+	return &BitcoinCashWallet{cfg.DB, km, params, c, wm, fp, mPrivKey, mPubKey, exchangeRates}, nil
 }
 
 func bitcoinCashAddress(key *hd.ExtendedKey, params *chaincfg.Params) (btcutil.Address, error) {
@@ -115,8 +111,8 @@ func (w *BitcoinCashWallet) CurrencyCode() string {
 	}
 }
 
-func (w *BitcoinCashWallet) IsDust(amount int64) bool {
-	return txrules.IsDustAmount(btcutil.Amount(amount), 25, txrules.DefaultRelayFeePerKb)
+func (w *BitcoinCashWallet) IsDust(amount big.Int) bool {
+	return txrules.IsDustAmount(btcutil.Amount(amount.Int64()), 25, txrules.DefaultRelayFeePerKb)
 }
 
 func (w *BitcoinCashWallet) MasterPrivateKey() *hd.ExtendedKey {
@@ -147,30 +143,17 @@ func (w *BitcoinCashWallet) ChildKey(keyBytes []byte, chaincode []byte, isPrivat
 }
 
 func (w *BitcoinCashWallet) CurrentAddress(purpose wi.KeyPurpose) btcutil.Address {
-	key, err := w.km.GetCurrentKey(purpose)
-	if err != nil {
-		w.log.Errorf("Error generating current key: %s", err)
-	}
-	addr, err := w.km.KeyToAddress(key)
-	if err != nil {
-		w.log.Errorf("Error converting key to address: %s", err)
-	}
-	return addr
+	key, _ := w.km.GetCurrentKey(purpose)
+	addr, _ := w.km.KeyToAddress(key)
+	return btcutil.Address(addr)
 }
 
 func (w *BitcoinCashWallet) NewAddress(purpose wi.KeyPurpose) btcutil.Address {
-	key, err := w.km.GetNextUnused(purpose)
-	if err != nil {
-		w.log.Errorf("Error generating next unused key: %s", err)
-	}
-	addr, err := w.km.KeyToAddress(key)
-	if err != nil {
-		w.log.Errorf("Error converting key to address: %s", err)
-	}
-	if err := w.db.Keys().MarkKeyAsUsed(addr.ScriptAddress()); err != nil {
-		w.log.Errorf("Error marking key as used: %s", err)
-	}
-	return addr
+	i, _ := w.db.Keys().GetUnused(purpose)
+	key, _ := w.km.GenerateChildKey(purpose, uint32(i[1]))
+	addr, _ := w.km.KeyToAddress(key)
+	w.db.Keys().MarkKeyAsUsed(addr.ScriptAddress())
+	return btcutil.Address(addr)
 }
 
 func (w *BitcoinCashWallet) DecodeAddress(addr string) (btcutil.Address, error) {
@@ -193,10 +176,12 @@ func (w *BitcoinCashWallet) HasKey(addr btcutil.Address) bool {
 	return true
 }
 
-func (w *BitcoinCashWallet) Balance() (confirmed, unconfirmed int64) {
+func (w *BitcoinCashWallet) Balance() (wi.CurrencyValue, wi.CurrencyValue) {
 	utxos, _ := w.db.Utxos().GetAll()
 	txns, _ := w.db.Txns().GetAll(false)
-	return util.CalcBalance(utxos, txns)
+	c, u := util.CalcBalance(utxos, txns)
+	return wi.CurrencyValue{Value: *big.NewInt(c), Currency: BitcoinCashCurrencyDefinition},
+		wi.CurrencyValue{Value: *big.NewInt(u), Currency: BitcoinCashCurrencyDefinition}
 }
 
 func (w *BitcoinCashWallet) Transactions() ([]wi.Txn, error) {
@@ -250,7 +235,7 @@ func (w *BitcoinCashWallet) GetTransaction(txid chainhash.Hash) (wi.Txn, error) 
 			}
 			tout := wi.TransactionOutput{
 				Address: addr,
-				Value:   out.Value,
+				Value:   *big.NewInt(out.Value),
 				Index:   uint32(i),
 			}
 			outs = append(outs, tout)
@@ -264,11 +249,11 @@ func (w *BitcoinCashWallet) ChainTip() (uint32, chainhash.Hash) {
 	return w.ws.ChainTip()
 }
 
-func (w *BitcoinCashWallet) GetFeePerByte(feeLevel wi.FeeLevel) uint64 {
-	return w.fp.GetFeePerByte(feeLevel)
+func (w *BitcoinCashWallet) GetFeePerByte(feeLevel wi.FeeLevel) big.Int {
+	return *big.NewInt(int64(w.fp.GetFeePerByte(feeLevel)))
 }
 
-func (w *BitcoinCashWallet) Spend(amount int64, addr btcutil.Address, feeLevel wi.FeeLevel, referenceID string, spendAll bool) (*chainhash.Hash, error) {
+func (w *BitcoinCashWallet) Spend(amount big.Int, addr btcutil.Address, feeLevel wi.FeeLevel, referenceID string, spendAll bool) (*chainhash.Hash, error) {
 	var (
 		tx  *wire.MsgTx
 		err error
@@ -279,7 +264,7 @@ func (w *BitcoinCashWallet) Spend(amount int64, addr btcutil.Address, feeLevel w
 			return nil, err
 		}
 	} else {
-		tx, err = w.buildTx(amount, addr, feeLevel, nil)
+		tx, err = w.buildTx(amount.Int64(), addr, feeLevel, nil)
 		if err != nil {
 			return nil, err
 		}
@@ -298,32 +283,33 @@ func (w *BitcoinCashWallet) BumpFee(txid chainhash.Hash) (*chainhash.Hash, error
 	return w.bumpFee(txid)
 }
 
-func (w *BitcoinCashWallet) EstimateFee(ins []wi.TransactionInput, outs []wi.TransactionOutput, feePerByte uint64) uint64 {
+func (w *BitcoinCashWallet) EstimateFee(ins []wi.TransactionInput, outs []wi.TransactionOutput, feePerByte big.Int) big.Int {
 	tx := new(wire.MsgTx)
 	for _, out := range outs {
 		scriptPubKey, _ := bchutil.PayToAddrScript(out.Address)
-		output := wire.NewTxOut(out.Value, scriptPubKey)
+		output := wire.NewTxOut(out.Value.Int64(), scriptPubKey)
 		tx.TxOut = append(tx.TxOut, output)
 	}
 	estimatedSize := EstimateSerializeSize(len(ins), tx.TxOut, false, P2PKH)
-	fee := estimatedSize * int(feePerByte)
-	return uint64(fee)
+	fee := estimatedSize * int(feePerByte.Int64())
+	return *big.NewInt(int64(fee))
 }
 
-func (w *BitcoinCashWallet) EstimateSpendFee(amount int64, feeLevel wi.FeeLevel) (uint64, error) {
-	return w.estimateSpendFee(amount, feeLevel)
+func (w *BitcoinCashWallet) EstimateSpendFee(amount big.Int, feeLevel wi.FeeLevel) (big.Int, error) {
+	val, err := w.estimateSpendFee(amount.Int64(), feeLevel)
+	return *big.NewInt(int64(val)), err
 }
 
 func (w *BitcoinCashWallet) SweepAddress(ins []wi.TransactionInput, address *btcutil.Address, key *hd.ExtendedKey, redeemScript *[]byte, feeLevel wi.FeeLevel) (*chainhash.Hash, error) {
 	return w.sweepAddress(ins, address, key, redeemScript, feeLevel)
 }
 
-func (w *BitcoinCashWallet) CreateMultisigSignature(ins []wi.TransactionInput, outs []wi.TransactionOutput, key *hd.ExtendedKey, redeemScript []byte, feePerByte uint64) ([]wi.Signature, error) {
-	return w.createMultisigSignature(ins, outs, key, redeemScript, feePerByte)
+func (w *BitcoinCashWallet) CreateMultisigSignature(ins []wi.TransactionInput, outs []wi.TransactionOutput, key *hd.ExtendedKey, redeemScript []byte, feePerByte big.Int) ([]wi.Signature, error) {
+	return w.createMultisigSignature(ins, outs, key, redeemScript, feePerByte.Uint64())
 }
 
-func (w *BitcoinCashWallet) Multisign(ins []wi.TransactionInput, outs []wi.TransactionOutput, sigs1 []wi.Signature, sigs2 []wi.Signature, redeemScript []byte, feePerByte uint64, broadcast bool) ([]byte, error) {
-	return w.multisign(ins, outs, sigs1, sigs2, redeemScript, feePerByte, broadcast)
+func (w *BitcoinCashWallet) Multisign(ins []wi.TransactionInput, outs []wi.TransactionOutput, sigs1 []wi.Signature, sigs2 []wi.Signature, redeemScript []byte, feePerByte big.Int, broadcast bool) ([]byte, error) {
+	return w.multisign(ins, outs, sigs1, sigs2, redeemScript, feePerByte.Uint64(), broadcast)
 }
 
 func (w *BitcoinCashWallet) GenerateMultisigScript(keys []hd.ExtendedKey, threshold int, timeout time.Duration, timeoutKey *hd.ExtendedKey) (addr btcutil.Address, redeemScript []byte, err error) {
@@ -389,12 +375,12 @@ func (w *BitcoinCashWallet) DumpTables(wr io.Writer) {
 	fmt.Fprintln(wr, "Transactions-----")
 	txns, _ := w.db.Txns().GetAll(true)
 	for _, tx := range txns {
-		fmt.Fprintf(wr, "Hash: %s, Height: %d, Value: %d, WatchOnly: %t\n", tx.Txid, int(tx.Height), int(tx.Value), tx.WatchOnly)
+		fmt.Fprintf(wr, "Hash: %s, Height: %d, Value: %s, WatchOnly: %t\n", tx.Txid, int(tx.Height), tx.Value, tx.WatchOnly)
 	}
 	fmt.Fprintln(wr, "\nUtxos-----")
 	utxos, _ := w.db.Utxos().GetAll()
 	for _, u := range utxos {
-		fmt.Fprintf(wr, "Hash: %s, Index: %d, Height: %d, Value: %d, WatchOnly: %t\n", u.Op.Hash.String(), int(u.Op.Index), int(u.AtHeight), int(u.Value), u.WatchOnly)
+		fmt.Fprintf(wr, "Hash: %s, Index: %d, Height: %d, Value: %s, WatchOnly: %t\n", u.Op.Hash.String(), int(u.Op.Index), int(u.AtHeight), u.Value, u.WatchOnly)
 	}
 }
 
@@ -426,6 +412,7 @@ func (w *BitcoinCashWallet) Broadcast(tx *wire.MsgTx) error {
 		if err != nil {
 			return err
 		}
+		val, _ := strconv.ParseInt(u.Value, 10, 64)
 		input := model.Input{
 			Txid: in.PreviousOutPoint.Hash.String(),
 			Vout: int(in.PreviousOutPoint.Index),
@@ -435,8 +422,8 @@ func (w *BitcoinCashWallet) Broadcast(tx *wire.MsgTx) error {
 			Sequence: uint32(in.Sequence),
 			N:        n,
 			Addr:     addr.String(),
-			Satoshis: u.Value,
-			Value:    float64(u.Value) / util.SatoshisPerCoin(wi.BitcoinCash),
+			Satoshis: val,
+			Value:    float64(val) / util.SatoshisPerCoin(wi.BitcoinCash),
 		}
 		cTxn.Inputs = append(cTxn.Inputs, input)
 	}
